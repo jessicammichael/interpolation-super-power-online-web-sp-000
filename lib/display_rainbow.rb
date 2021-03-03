84964bcad33fc7a5aa764f330d3efdf5565080ce
Jessica Michael <pantherjmm@yahoo.com>
2021-03-03 04:45:53 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,4 +4,10 @@ def display_rainbow(colors)
   puts colors
 end
 
-display_rainbow("R: red" + "O: orange")
+display_rainbow("R: red")
+display_rainbow("O: orange")
+display_rainbow("Y: yellow")
+display_rainbow("G: green")
+display_rainbow("B: blue")
+display_rainbow("I: indigo")
+display_rainbow("V: violet"
