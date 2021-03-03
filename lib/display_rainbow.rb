6846a8c14a065e6cd3ade745c04c5259b351d0c9
Jessica Michael <pantherjmm@yahoo.com>
2021-03-03 04:44:45 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,13 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-  [R: red, + O: orange, + Y: yellow, + G: green, + B: blue, + I: indigo, + V: violet].each do |n|  
-  end
-  text = "Order of rainbow is: #{n}"
-  puts text
+  puts colors
 end
+
+display_rainbow("R: red")
+display_rainbow("O: orange")
+display_rainbow("Y: yellow")
+display_rainbow("G: green")
+display_rainbow("B: blue")
+display_rainbow("I: indigo")
+display_rainbow("V: violet")
